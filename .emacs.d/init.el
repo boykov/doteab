172890f9767ed965496a3619f7f174ce3e1adab2
Evgeny Boykov <artscan@list.ru>
2015-01-15 20:44:33 +1000

    add init.el and bootstrap.el: el-get

---
+++ .emacs.d/init.el
@@ -0,0 +1,10 @@
+;;; init.el --- 
+
+;; Copyright (C) 2010, 2011, 2012, 2013, 2014, 2015 Evgeny Boykov
+;;
+;; Author: artscan@list.ru
+;; Keywords: init, dotemacs
+;; Requirements: 
+;; Status: not intended to be distributed yet
+
+(load (concat user-emacs-directory "lisp/bootstrap.el"))
---
+++ .emacs.d/lisp/bootstrap.el
@@ -0,0 +1,21 @@
+;;; bootstrap.el --- 
+
+;; Copyright (C) 2010, 2011, 2012, 2013, 2014, 2015 Evgeny Boykov
+;;
+;; Author: artscan@list.ru
+;; Keywords: bootstrap, dotemacs
+;; Requirements: el-get
+;; Status: not intended to be distributed yet
+
+(add-to-list 'load-path (concat user-emacs-directory "el-get/el-get"))
+
+(setq el-get-emacswiki-elisp-file-list-url (concat "file://" (getenv "HOME") "/cache/emacswiki.html"))
+(setq el-get-git-install-url (concat (getenv "HOME") "/cache/el-get"))
+
+(unless (require 'el-get nil t)
+  (setq el-get-install-branch "master")
+  (switch-to-buffer
+   (url-retrieve-synchronously
+    (concat "file://" (getenv "HOME") "/cache/el-get-install.el")))
+  (end-of-buffer)
+  (eval-print-last-sexp))
