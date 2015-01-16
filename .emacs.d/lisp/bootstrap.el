8c3440fa7f485494313821332b20cb48b5e3f30c
Evgeny Boykov <artscan@list.ru>
2015-01-16 11:19:52 +1000

    fix cache: add automation, eabrecipes/cache

--- .emacs.d/lisp/bootstrap.el
+++ .emacs.d/lisp/bootstrap.el
@@ -20,9 +20,31 @@
   (end-of-buffer)
   (eval-print-last-sexp))
 
+(setq package-archives
+      '(("melpa" . "http://melpa.milkbox.net/packages/")
+	("elpa" . "http://tromey.com/elpa/")
+	("gnu" . "http://elpa.gnu.org/packages/")))
+
+(setq package-user-dir
+      (expand-file-name
+       (convert-standard-filename
+	(concat
+	 (file-name-as-directory
+	  (el-get-package-directory "package"))
+	 "elpa")))
+      package-directory-list
+      (list
+       (file-name-as-directory package-user-dir)
+       "/usr/share/emacs/site-lisp/elpa/"))
+
+(if (not (file-exists-p (concat (getenv "HOME") "/cache/elparcp")))
+    (progn
+      (make-directory (concat (getenv "HOME") "/cache/elparcp"))
+      (el-get-elpa-build-local-recipes (concat (getenv "HOME") "/cache/elparcp"))))
+
 (setq el-get-recipe-path nil)
 (add-to-list 'el-get-recipe-path (concat (getenv "HOME") "/cache/elparcp"))
-(add-to-list 'el-get-recipe-path (concat (getenv "HOME") "/cache/eabrecipes"))
+(add-to-list 'el-get-recipe-path (concat user-emacs-directory "eabrecipes/cache"))
 
 (if (not (el-get-package-installed-p 'eab-misc))
     (el-get 'sync '(eab-misc)))
